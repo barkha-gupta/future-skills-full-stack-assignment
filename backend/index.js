2a33f3a7003d9f6f90f6433b04c6bcb4d9402e62
barkha-gupta <barkhagupta401@gmail.com>
2024-08-29 04:50:31 +0530

    feat(backend): add cors middleware

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const rootRouter = require("./routes/index");
-
+const cors = require("cors");
 const app = express();
 dotenv.config();
 
-app.use(express.json());
-app.use("/api", rootRouter);
-
 const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO_URI;
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", rootRouter);
 
 app.get("/", (req, res) => {
   res.send("hi there!");
