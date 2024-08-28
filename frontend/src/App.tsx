34c1a40d10d2f88e8b41d2cf3ff9a0b959e392b1
barkha-gupta <barkhagupta401@gmail.com>
2024-08-29 03:47:08 +0530

    feat(frontend): add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-red-500 font-bold">Hello world!</h1>
+      <Header />
     </>
   );
 }
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Logo from "../assets/logo.png";
+const Header: React.FC = () => {
+  return (
+    <div className="bg-black border-t rounded-t-2xl text-[#e7e8e7] flex items-center justify-between px-12 py-4">
+      <div className="flex items-center gap-2">
+        <span>
+          <img src={Logo} alt="logo" className="w-8 h-8" />
+        </span>
+        <span className="text-lg text-[#eaeaea] font-semibold">Abstract</span>
+        <span className="font-semibold">|</span>
+        <span>Help Center</span>
+      </div>
+
+      <div>
+        <button className="border-2 border-[#585858] rounded-md px-3 py-2 hover:font-medium">
+          Submit a request
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
