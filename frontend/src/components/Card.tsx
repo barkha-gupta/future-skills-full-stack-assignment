5d2e81d4a252a3518aea803c01d56010fb493df6
barkha-gupta <barkhagupta401@gmail.com>
2024-08-29 05:31:46 +0530

    feat(frontend): add card component

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,5 +1,17 @@
-const Card = () => {
-  return <div>Card</div>;
+import { CardItem } from "./Hero";
+
+interface CardProps {
+  item: CardItem;
+}
+const Card: React.FC<CardProps> = ({ item }) => {
+  return (
+    <div className="border bg-[#f4f6f8] flex flex-col text-[#3c4349] rounded-md w-[350px] shadow-sm">
+      <div className="font-semibold border-b border-b-slate-300 px-4 py-2">
+        {item.title}
+      </div>
+      <div className="px-4 pt-2 pb-4">{item.description}</div>
+    </div>
+  );
 };
 
 export default Card;
--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import Search from "./Search";
+import Card from "./Card";
+
+export interface CardItem {
+  _id: string;
+  title: string;
+  description: string;
+}
 
 const Hero: React.FC = () => {
+  const [cards, setCards] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/cards")
+      .then((res) => res.json())
+      .then((data) => setCards(data.cards))
+      .catch((error) => {
+        console.log("Error occurred while fetching cards:", error);
+      });
+  }, []);
+
   return (
     <>
       <Search />
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-20 gap-y-20 p-20 place-items-center">
+        {cards.map((item: CardItem) => (
+          <Card key={item._id} item={item} />
+        ))}
+      </div>
     </>
   );
 };
